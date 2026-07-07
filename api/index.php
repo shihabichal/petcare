2554cec28100dc98bfcf16d0b1fa89f8cce121eb
Shihab Ichal <sxnsyw@gmail.com>
2026-07-08 00:09:56 +0700

    Move bootstrap cache to /tmp for Vercel

--- api/index.php
+++ api/index.php
@@ -2,7 +2,17 @@
 
 /**
  * Vercel Serverless Entrypoint for Laravel
- */
+// Fix Vercel read-only filesystem issues for bootstrap/cache
+$tmpCacheDir = '/tmp/storage/bootstrap/cache';
+if (!is_dir($tmpCacheDir)) {
+    mkdir($tmpCacheDir, 0755, true);
+}
+
+$_ENV['APP_SERVICES_CACHE'] = $_SERVER['APP_SERVICES_CACHE'] = $tmpCacheDir . '/services.php';
+$_ENV['APP_PACKAGES_CACHE'] = $_SERVER['APP_PACKAGES_CACHE'] = $tmpCacheDir . '/packages.php';
+$_ENV['APP_CONFIG_CACHE']   = $_SERVER['APP_CONFIG_CACHE']   = $tmpCacheDir . '/config.php';
+$_ENV['APP_ROUTES_CACHE']   = $_SERVER['APP_ROUTES_CACHE']   = $tmpCacheDir . '/routes.php';
+$_ENV['APP_EVENTS_CACHE']   = $_SERVER['APP_EVENTS_CACHE']   = $tmpCacheDir . '/events.php';
 
 try {
     require __DIR__ . '/../public/index.php';
