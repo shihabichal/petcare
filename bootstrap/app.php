c9e1b7cb0a6116ff09706462145340bcbbe7e9cf
Shihab Ichal <sxnsyw@gmail.com>
2026-07-08 00:15:55 +0700

    Remove all Vercel configurations and restore original files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -4,7 +4,7 @@ use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
-$app = Application::configure(basePath: dirname(__DIR__))
+return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
@@ -17,33 +17,5 @@ $app = Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
-        $exceptions->reportable(function (\Throwable $e) {
-            if (isset($_ENV['IS_VERCEL'])) {
-                echo "<h1>🔥 ORIGINAL ERROR: " . $e->getMessage() . "</h1>";
-                echo "<pre>" . $e->getTraceAsString() . "</pre>";
-                exit;
-            }
-        });
+        //
     })->create();
-
-if (isset($_ENV['IS_VERCEL'])) {
-    $app->useStoragePath('/tmp/storage');
-    
-    // Create required directories since /tmp is empty on boot
-    $directories = [
-        '/tmp/storage/framework/views',
-        '/tmp/storage/framework/cache',
-        '/tmp/storage/framework/cache/data',
-        '/tmp/storage/framework/sessions',
-        '/tmp/storage/logs',
-        '/tmp/storage/app/public',
-    ];
-    
-    foreach ($directories as $dir) {
-        if (!is_dir($dir)) {
-            mkdir($dir, 0755, true);
-        }
-    }
-}
-
-return $app;
