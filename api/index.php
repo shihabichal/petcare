d387c0e0c32f907c36fa41c1457c21a5d7294418
Shihab Ichal <sxnsyw@gmail.com>
2026-07-08 00:05:37 +0700

    Catch exact exception in Vercel to bypass View missing error

--- api/index.php
+++ api/index.php
@@ -4,4 +4,11 @@
  * Vercel Serverless Entrypoint for Laravel
  */
 
-require __DIR__ . '/../public/index.php';
+try {
+    require __DIR__ . '/../public/index.php';
+} catch (\Throwable $e) {
+    echo "<h1>🔥 Fatal Error in Vercel:</h1>";
+    echo "<p style='color:red;'><b>" . $e->getMessage() . "</b></p>";
+    echo "<h3>Stack Trace:</h3>";
+    echo "<pre style='background:#f4f4f4;padding:15px;overflow:auto;'>" . $e->getTraceAsString() . "</pre>";
+}
